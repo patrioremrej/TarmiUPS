775fb92bebcd29633b5f605178d74a59f97344d3
patrioremrej <patrioremrej@gmail.com>
2025-08-29 10:18:09 +0530

    Update token_manager.py

--- bot/helper/ext_utils/token_manager.py
+++ bot/helper/ext_utils/token_manager.py
@@ -202,7 +202,7 @@ async def start(client, message):
     ):
         start_string = "Bot Started.\n" \
                        "Now I will send all of your stuffs here.\n" \
-                       "Use me at: @Z_Mirror"
+                       "Use me at: @EchoBotz"
     elif (
         not config_dict["DM_MODE"]
         and message.chat.type != message.chat.type.SUPERGROUP
